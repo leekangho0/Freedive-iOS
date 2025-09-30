8e1a8559604b68e48e77aa277c9e3d8138bb277a
kangho lee <ibcylon@naver.com>
2025-10-01 00:57:36 +0900

    add: stats View

--- Freedive/Features/Stats/StatsView.swift
+++ Freedive/Features/Stats/StatsView.swift
@@ -9,7 +9,64 @@ import SwiftUI
 
 struct StatsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack(spacing: 20) {
+                VStack {
+                    Text("Your Stats")
+                        .font(.largeTitle)
+
+                    Text("Personal records and achievements")
+                        .font(.subheadline)
+                        .foregroundStyle(.secondary)
+                }
+
+                HStack {
+                    CardView(icon: "wave", value: "1", description: "Total Dives")
+
+                    CardView(icon: "wave", value: "1", description: "Total Dives")
+                }
+
+                HStack {
+                    CardView(icon: "wave", value: "1", description: "Total Dives")
+
+                    CardView(icon: "wave", value: "1", description: "Total Dives")
+                }
+
+                Text("Preferences")
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .frame(minHeight: 150)
+                    .background {
+                        RoundedRectangle(cornerRadius: 20)
+                            .fill(.white)
+                            .stroke(Color.gray, lineWidth: 1)
+                    }
+                    .padding(.horizontal)
+            }
+        }
+    }
+}
+
+struct CardView: View {
+    let icon: String
+    let value: String
+    let description: String
+
+    var body: some View {
+        VStack(spacing: 12) {
+            Image(systemName: "water.waves")
+                .foregroundStyle(Color.indigo)
+
+            Text(value)
+
+            Text(description)
+        }
+        .padding()
+        .background {
+            RoundedRectangle(cornerRadius: 20)
+                .fill(.white)
+                .stroke(Color.gray, lineWidth: 1)
+        }
     }
 }
 
